c569451ccf4fec69b68e80c651a5c345fe4cc6ac
mcleancm <colemichael999@gmail.com>
2021-03-01 09:40:18 -0700

    Madlibs code added

--- src/Madlibs.java
+++ src/Madlibs.java
@@ -1,6 +1,18 @@
-public class Madlibs {
 
+import java.util.Scanner; // import the Scanner class
+
+public class Madlibs {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner userInput = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Please Enter Your Name");
+        String Name = userInput.nextLine();
+        System.out.println("Welcome" + Name + "Fill out everything to create some cool madlibs!!");
+        System.out.println("Please enter a noun?");
+        String noun1 = userInput.nextLine();
+        System.out.println("Your noun is" + noun1 +"");
+        System.out.println("Thank you. Now, enter a verb.");
+        String verb1 = userInput.nextLine();
+        System.out.println("Your verb is" + verb1 +"");
+        System.out.println("You are a spelunker and your name is David.  You like " + noun1 +". But you always run into "+verb1);  // Output user input
     }
 }
