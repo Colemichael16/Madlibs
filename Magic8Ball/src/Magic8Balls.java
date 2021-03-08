ef8ea62a646eeb240cce851cbdaf045b81dd3765
mcleancm <colemichael999@gmail.com>
2021-03-08 09:40:09 -0700

    FinishedMadlibs

--- Magic8Ball/src/Magic8Balls.java
+++ Magic8Ball/src/Magic8Balls.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 
 public class Magic8Balls {
     public static void main (String[] args) {
-        String[] Answers = {"Yes", "No", "Try again later", "Very Doubtful", "Better not tell you now", "Yes - %100", "Don't count on it", "Without a doubt"};
+        System.out.println("Welcome to magic 8 ball! Ask me a question!");
+        String[] Answers = { "It is certain.", "It is decidedly so.", "Without a doubt.", "Yes – definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.",
+                "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.",
+                "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."
+        };
         Scanner scan = new Scanner(System.in);
         String question = scan.nextLine();
 
@@ -11,8 +15,8 @@ public class Magic8Balls {
         while (true) {
             Random randNum = new Random();
             int num = randNum.nextInt(20);
-
-            System.out.println("Do you have any questions? Type yes or no.");
+            System.out.println(Answers[num]);
+            System.out.println("Do you have any more questions? Type yes or no.");
             String answer = scan.nextLine();
 
             if (answer.equalsIgnoreCase("no")) {
