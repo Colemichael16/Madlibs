d52a9d49ffe0c5deb3b38f6890961463ace294b9
mcleancm <colemichael999@gmail.com>
2021-03-08 09:32:09 -0700

    Madlibs code finished and added 8ball

--- src/Madlibs.java
+++ src/Madlibs.java
@@ -2,7 +2,7 @@
 import java.util.Scanner; // import the Scanner class
 
 public class Madlibs {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Scanner userInput = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Please Enter Your Name");
         String Name = userInput.nextLine();
@@ -24,8 +24,12 @@ public class Madlibs {
         System.out.println("Your noun is " + Noun +"");
         System.out.println("Now tell me an adjective");
         String Adjective = userInput.nextLine();
-        System.out.println("Your adjective is" + Adjective + "");
-
-        System.out.println("Your name is " + Name + ". You are currently at a " + Place + " doing " + fun + " You suddenly see a dog lying on the floor so you " + verb1 + " the dog. The dog then gets really angry and decides to bite you! So you " + verb2 + " the dog, it runs away scared. You then head over to see " + Noun + " .While headding there you start to feel " + Adjective + "");  // Output user input
+        System.out.println("Your adjective is " + Adjective + "");
+        System.out.println("Tell me a noun!");
+        String Noun3 = userInput.nextLine();
+        System.out.println("Your noun is " +Noun3+"");
+        System.out.println("Now generating madlibs give us a few moments.");
+        Thread.sleep(5000);
+        System.out.println("Your name is " + Name + ". You are currently at a " + Place + " doing " + fun + " You suddenly see a dog lying on the floor so you " + verb1 + " the dog. The dog then gets really angry and decides to bite you! So you " + verb2 + " the dog, it runs away scared. You then head over to see " + Noun + ". While heading there you start to feel " + Adjective + " Then suddenly you turn into a " +Noun3+ ". You then start to scream. And then you wake up, everything was a dream. You are relieved and you go back to bed. The End. Thank you for playing " +Name+ ".");  // Output user input
     }
 }
